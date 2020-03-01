5e93af9f43edfbfc0e5991809f03b7b24155db7f
whois-sera <whois.sera.alt@gmail.com>
2020-03-01 11:58:08 +0100

    Clean JS folder and set dynamic age calculation in about section

--- scripts/interaction.js
+++ scripts/interaction.js
@@ -36,4 +36,19 @@ $(document).ready(function () {
     links.click(function () {
         offCanevas.toggleClass('visible');
     })
+
+    function getAge(dateString) {
+        var today = new Date();
+        var birthDate = new Date(dateString);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+    $age = $('#age');
+    $age.text(getAge("1994/03/27"));
+
 });
